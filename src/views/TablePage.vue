d3180a139b456c8b81c7ad2f9e0c5b488dd38053
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2024-03-20 13:52:13 +0700

    More dummy info

--- src/views/TablePage.vue
+++ src/views/TablePage.vue
@@ -69,9 +69,27 @@ export default {
       premierLeagueStandings: [
         {
           team: {
+            id: 6,
+            name: "Arsenal",
+            crest:
+              "https://upload.wikimedia.org/wikipedia/en/5/53/Arsenal_FC.svg",
+          },
+          playedGames: 10,
+          won: 8,
+          draw: 2,
+          lost: 0,
+          goalsFor: 13,
+          goalsAgainst: 14,
+          goalDifference: 18,
+          form: "WLWDL",
+          points: 26,
+        },
+        {
+          team: {
             id: 1,
             name: "Manchester United",
-            crest: "/path/to/manchester_united_crest.png",
+            crest:
+              "https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg",
           },
           playedGames: 10,
           won: 7,
@@ -87,7 +105,8 @@ export default {
           team: {
             id: 2,
             name: "Liverpool",
-            crest: "/path/to/liverpool_crest.png",
+            crest:
+              "https://upload.wikimedia.org/wikipedia/en/0/0c/Liverpool_FC.svg",
           },
           playedGames: 10,
           won: 6,
@@ -101,9 +120,27 @@ export default {
         },
         {
           team: {
+            id: 3,
+            name: "Chelsea",
+            crest:
+              "https://upload.wikimedia.org/wikipedia/en/c/cc/Chelsea_FC.svg",
+          },
+          playedGames: 10,
+          won: 5,
+          draw: 4,
+          lost: 1,
+          goalsFor: 15,
+          goalsAgainst: 8,
+          goalDifference: 7,
+          form: "DDWWL",
+          points: 19,
+        },
+        {
+          team: {
             id: 20,
             name: "West Ham United",
-            crest: "/path/to/west_ham_united_crest.png",
+            crest:
+              "https://upload.wikimedia.org/wikipedia/en/e/e0/West_Ham_United_FC.svg",
           },
           playedGames: 10,
           won: 3,
@@ -115,7 +152,60 @@ export default {
           form: "LDWLW",
           points: 13,
         },
+        {
+          team: {
+            id: 4,
+            name: "Manchester City",
+            crest:
+              "https://upload.wikimedia.org/wikipedia/en/e/eb/Manchester_City_FC_badge.svg",
+          },
+          playedGames: 10,
+          won: 4,
+          draw: 4,
+          lost: 2,
+          goalsFor: 16,
+          goalsAgainst: 10,
+          goalDifference: 6,
+          form: "WLDDL",
+          points: 16,
+        },
+        {
+          team: {
+            id: 5,
+            name: "Tottenham Hotspur",
+            crest:
+              "https://upload.wikimedia.org/wikipedia/en/b/b4/Tottenham_Hotspur.svg",
+          },
+          playedGames: 10,
+          won: 3,
+          draw: 5,
+          lost: 2,
+          goalsFor: 14,
+          goalsAgainst: 12,
+          goalDifference: 2,
+          form: "DLDWW",
+          points: 14,
+        },
+
+        {
+          team: {
+            id: 7,
+            name: "Leicester City",
+            crest:
+              "https://upload.wikimedia.org/wikipedia/en/2/2d/Leicester_City_crest.svg",
+          },
+          playedGames: 10,
+          won: 3,
+          draw: 4,
+          lost: 3,
+          goalsFor: 15,
+          goalsAgainst: 17,
+          goalDifference: -2,
+          form: "DLWWD",
+          points: 13,
+        },
       ],
+
       isLoading: false,
     };
   },
@@ -141,7 +231,8 @@ export default {
 
 .team-crest {
   width: 24px;
-  height: 24px;
+  height: auto;
+  max-height: 24px;
   margin-right: 8px;
 }
 </style>
