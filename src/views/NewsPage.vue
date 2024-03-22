e511c26ea622bab981793e820a74da3fd6d2b44b
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2024-03-22 10:02:50 +0700

    Picsum dummy images

--- src/views/NewsPage.vue
+++ src/views/NewsPage.vue
@@ -22,8 +22,7 @@
         <!-- Show all articles -->
         <div v-for="article in articles" :key="article.id" class="article">
           <img
-            v-if="article.image"
-            :src="article.image"
+            :src="getRandomImage()"
             alt="Article Image"
             class="article-image"
           />
@@ -41,8 +40,7 @@
           class="article"
         >
           <img
-            v-if="article.image"
-            :src="article.image"
+            :src="getRandomImage()"
             alt="Article Image"
             class="article-image"
           />
@@ -74,28 +72,24 @@ export default {
           title: "Article 1",
           description: "Description of article 1",
           content: "Content of article 1",
-          image: "https://via.placeholder.com/150",
         },
         {
           id: 2,
           title: "Article 2",
           description: "Description of article 2",
           content: "Content of article 2",
-          image: "https://via.placeholder.com/150",
         },
         {
           id: 3,
           title: "Article 3",
           description: "Description of article 3",
           content: "Content of article 3",
-          image: "https://via.placeholder.com/150",
         },
         {
           id: 4,
           title: "Article 4",
           description: "Description of article 4",
           content: "Content of article 4",
-          image: "https://via.placeholder.com/150",
         },
         // Add more articles as needed
       ],
@@ -112,6 +106,14 @@ export default {
           (article) => article.teamId === this.selectedTeam
         );
       }
+    },
+  },
+  methods: {
+    getRandomImage() {
+      // Generate random image URL from picsum.photos
+      const randomWidth = Math.floor(Math.random() * (600 - 300 + 1)) + 300;
+      const randomHeight = Math.floor(Math.random() * (400 - 200 + 1)) + 200;
+      return `https://picsum.photos/${randomWidth}/${randomHeight}`;
     },
   },
 };
@@ -148,7 +150,8 @@ export default {
 
 .article-image {
   width: 100%;
-  height: auto;
+  height: 150px;
+  object-fit: cover;
   border-radius: 5px 5px 0 0;
 }
 
