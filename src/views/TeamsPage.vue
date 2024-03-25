a614fcf67309a53078cd3b3ee20263516bd7632d
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2024-03-25 12:57:43 +0700

    Teams page initial layout

--- src/views/TeamsPage.vue
+++ src/views/TeamsPage.vue
@@ -1,3 +1,202 @@
 <template>
-  <div class="heading-container"><h2>Teams</h2></div>
+  <div>
+    <div class="heading-container">
+      <h2>Teams</h2>
+    </div>
+    <div class="teams-list">
+      <div
+        v-for="team in teams"
+        :key="team.id"
+        class="team-item"
+        @click="navigateToTeamPage(team)"
+      >
+        <span class="crest-container">
+          <img :src="team.crest" :alt="team.name" class="team-crest" />
+        </span>
+        <div class="team-name">{{ team.name }}</div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "TeamsPage",
+  data() {
+    return {
+      teams: [
+        {
+          id: 1,
+          name: "Arsenal",
+          crest:
+            "https://upload.wikimedia.org/wikipedia/en/5/53/Arsenal_FC.svg",
+        },
+        {
+          id: 2,
+          name: "Manchester City",
+          crest:
+            "https://upload.wikimedia.org/wikipedia/en/e/eb/Manchester_City_FC_badge.svg",
+        },
+        {
+          id: 3,
+          name: "Manchester United",
+          crest:
+            "https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg",
+        },
+        {
+          id: 4,
+          name: "Liverpool",
+          crest:
+            "https://upload.wikimedia.org/wikipedia/en/0/0c/Liverpool_FC.svg",
+        },
+        {
+          id: 5,
+          name: "Chelsea",
+          crest:
+            "https://upload.wikimedia.org/wikipedia/en/c/cc/Chelsea_FC.svg",
+        },
+        {
+          id: 6,
+          name: "Tottenham Hotspur",
+          crest:
+            "https://upload.wikimedia.org/wikipedia/en/b/b4/Tottenham_Hotspur.svg",
+        },
+        {
+          id: 7,
+          name: "Leicester City",
+          crest:
+            "https://upload.wikimedia.org/wikipedia/en/2/2d/Leicester_City_crest.svg",
+        },
+        {
+          id: 8,
+          name: "Everton",
+          crest:
+            "https://upload.wikimedia.org/wikipedia/en/7/7c/Everton_FC_logo.svg",
+        },
+        {
+          id: 9,
+          name: "Aston Villa",
+          crest:
+            "https://upload.wikimedia.org/wikipedia/de/9/9f/Aston_Villa_logo.svg",
+        },
+        {
+          id: 10,
+          name: "Leeds United",
+          crest:
+            "https://upload.wikimedia.org/wikipedia/en/5/54/Leeds_United_F.C._logo.svg",
+        },
+        {
+          id: 11,
+          name: "Southampton",
+          crest:
+            "https://upload.wikimedia.org/wikipedia/en/c/c9/FC_Southampton.svg",
+        },
+        {
+          id: 12,
+          name: "Brighton & Hove Albion",
+          crest:
+            "https://upload.wikimedia.org/wikipedia/en/f/fd/Brighton_%26_Hove_Albion_logo.svg",
+        },
+        {
+          id: 13,
+          name: "Wolverhampton Wanderers",
+          crest:
+            "https://upload.wikimedia.org/wikipedia/en/f/fc/Wolverhampton_Wanderers.svg",
+        },
+        {
+          id: 14,
+          name: "Crystal Palace",
+          crest:
+            "https://upload.wikimedia.org/wikipedia/sco/0/0c/Crystal_Palace_FC_logo.svg",
+        },
+        {
+          id: 15,
+          name: "Newcastle United",
+          crest:
+            "https://upload.wikimedia.org/wikipedia/en/5/56/Newcastle_United_Logo.svg",
+        },
+        {
+          id: 16,
+          name: "Burnley",
+          crest:
+            "https://upload.wikimedia.org/wikipedia/fr/f/f3/Logo_Burnley_FC_1960.svg",
+        },
+        {
+          id: 17,
+          name: "Fulham",
+          crest:
+            "https://upload.wikimedia.org/wikipedia/en/e/eb/Fulham_FC_%28shield%29.svg",
+        },
+        {
+          id: 18,
+          name: "West Bromwich Albion",
+          crest:
+            "https://upload.wikimedia.org/wikipedia/en/8/8b/West_Bromwich_Albion.svg",
+        },
+        {
+          id: 19,
+          name: "Sheffield United",
+          crest:
+            "https://upload.wikimedia.org/wikipedia/en/9/9c/Sheffield_United_FC_logo.svg",
+        },
+        {
+          id: 20,
+          name: "West Ham United",
+          crest:
+            "https://upload.wikimedia.org/wikipedia/en/e/e0/West_Ham_United_FC.svg",
+        },
+      ],
+    };
+  },
+  mounted() {
+    // Sort teams alphabetically by name
+    this.teams.sort((a, b) => a.name.localeCompare(b.name));
+  },
+  methods: {
+    navigateToTeamPage(team) {
+      // Logic to navigate to the selected team's page
+      console.log("Navigating to", team.name);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.teams-list {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  gap: 20px;
+  padding: 20px;
+}
+
+.team-item {
+  background-color: #f8f8f8;
+  padding: 10px;
+  border-radius: 4px;
+  border: 1px solid #ccc;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.team-item:hover {
+  background-color: #e0e0e0;
+}
+
+.crest-container {
+  margin-bottom: 10px;
+}
+
+.team-crest {
+  max-width: 100px;
+  width: auto;
+  max-height: 100px;
+}
+
+.team-name {
+  font-weight: bold;
+  text-align: center;
+}
+</style>
