dff6e958f13fbce8e5658d32b4abe179dd2313b1
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2024-03-25 07:52:10 +0700

    Loading crest info

--- src/views/ResultsPage.vue
+++ src/views/ResultsPage.vue
@@ -23,11 +23,11 @@
           class="fixture-item"
         >
           <div class="team-container team-left">
-            <!-- <img
+            <img
               :src="getTeamCrest(result.homeTeam.id)"
               :alt="result.homeTeam.name"
               class="team-crest"
-            /> -->
+            />
             <div class="team-name">{{ result.homeTeam.name }}</div>
           </div>
           <div class="score-container">
@@ -38,11 +38,11 @@
           </div>
           <div class="team-container team-right">
             <div class="team-name">{{ result.awayTeam.name }}</div>
-            <!-- <img
+            <img
               :src="getTeamCrest(result.awayTeam.id)"
               :alt="result.awayTeam.name"
               class="team-crest"
-            /> -->
+            />
           </div>
         </div>
       </div>
@@ -75,46 +75,110 @@ export default {
   },
   methods: {
     getTeamCrest(teamId) {
-      return this.teamCrestsData[teamId] || "placeholder.jpg";
+      // Find the team with the matching ID in the results array
+      const team = this.results.find(
+        (result) =>
+          result.homeTeam.id === teamId || result.awayTeam.id === teamId
+      );
+      // Check if the team is found
+      if (team) {
+        // Return the crest URL of the found team
+        return team.homeTeam.id === teamId
+          ? team.homeTeam.crest
+          : team.awayTeam.crest;
+      } else {
+        // If the team is not found, return a placeholder image
+        return "placeholder.jpg";
+      }
     },
     async fetchResults() {
       this.results = [
         {
           id: 1,
-          homeTeam: { id: 1, name: "Arsenal" },
-          awayTeam: { id: 2, name: "Manchester City" },
+          homeTeam: {
+            id: 1,
+            name: "Arsenal",
+            crest:
+              "https://upload.wikimedia.org/wikipedia/en/5/53/Arsenal_FC.svg",
+          },
+          awayTeam: {
+            id: 2,
+            name: "Manchester City",
+            crest:
+              "https://upload.wikimedia.org/wikipedia/en/e/eb/Manchester_City_FC_badge.svg",
+          },
           matchday: 1,
           status: "FINISHED",
           score: { fullTime: { home: 2, away: 1 } },
         },
         {
           id: 2,
-          homeTeam: { id: 3, name: "Manchester United" },
-          awayTeam: { id: 4, name: "Liverpool" },
+          homeTeam: {
+            id: 3,
+            name: "Manchester United",
+            crest:
+              "https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg",
+          },
+          awayTeam: {
+            id: 4,
+            name: "Liverpool",
+            crest:
+              "https://upload.wikimedia.org/wikipedia/en/0/0c/Liverpool_FC.svg",
+          },
           matchday: 1,
           status: "FINISHED",
           score: { fullTime: { home: 0, away: 0 } },
         },
         {
           id: 3,
-          homeTeam: { id: 2, name: "Manchester City" },
-          awayTeam: { id: 3, name: "Manchester United" },
+          homeTeam: {
+            id: 2,
+            name: "Manchester City",
+            crest:
+              "https://upload.wikimedia.org/wikipedia/en/e/eb/Manchester_City_FC_badge.svg",
+          },
+          awayTeam: {
+            id: 3,
+            name: "Manchester United",
+            crest:
+              "https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg",
+          },
           matchday: 2,
           status: "FINISHED",
           score: { fullTime: { home: 1, away: 1 } },
         },
         {
           id: 4,
-          homeTeam: { id: 1, name: "Arsenal" },
-          awayTeam: { id: 4, name: "Liverpool" },
+          homeTeam: {
+            id: 1,
+            name: "Arsenal",
+            crest:
+              "https://upload.wikimedia.org/wikipedia/en/5/53/Arsenal_FC.svg",
+          },
+          awayTeam: {
+            id: 4,
+            name: "Liverpool",
+            crest:
+              "https://upload.wikimedia.org/wikipedia/en/0/0c/Liverpool_FC.svg",
+          },
           matchday: 2,
           status: "FINISHED",
           score: { fullTime: { home: 3, away: 2 } },
         },
         {
           id: 5,
-          homeTeam: { id: 2, name: "Manchester City" },
-          awayTeam: { id: 4, name: "Liverpool" },
+          homeTeam: {
+            id: 2,
+            name: "Manchester City",
+            crest:
+              "https://upload.wikimedia.org/wikipedia/en/e/eb/Manchester_City_FC_badge.svg",
+          },
+          awayTeam: {
+            id: 4,
+            name: "Liverpool",
+            crest:
+              "https://upload.wikimedia.org/wikipedia/en/0/0c/Liverpool_FC.svg",
+          },
           matchday: 3,
           status: "FINISHED",
           score: { fullTime: { home: 2, away: 2 } },
