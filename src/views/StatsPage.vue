e5b92581591a6d5d30f5e65bde0e6f26d51e1666
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2024-03-27 20:14:18 +0700

    Statistics Heading

--- src/views/StatsPage.vue
+++ src/views/StatsPage.vue
@@ -1,3 +1,17 @@
 <template>
-  <div>Stats Page</div>
+  <div class="heading-container">
+    <h2 v-if="teamName != null">{{ teamName }} Statistics</h2>
+    <h2 v-else>Latest Football Videos</h2>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {},
+  computed: {
+    teamName() {
+      return this.$store.state.selectedTeam || "Arsenal";
+    },
+  },
+};
+</script>
