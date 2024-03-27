98ed9cf683abb21369df14161876a4673638c69d
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2024-03-27 20:14:41 +0700

    Squad page heading

--- src/views/SquadPage.vue
+++ src/views/SquadPage.vue
@@ -1,3 +1,17 @@
 <template>
-  <div>Squad</div>
+  <div class="heading-container">
+    <h2 v-if="teamName != null">{{ teamName }} Squad</h2>
+    <h2 v-else>Latest Football Videos</h2>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {},
+  computed: {
+    teamName() {
+      return this.$store.state.selectedTeam || "Arsenal";
+    },
+  },
+};
+</script>
