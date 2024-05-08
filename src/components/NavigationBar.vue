6615df6c6b818becc48d314518b36af6609b3023
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2024-05-08 08:11:42 +0700

    Dropdown appearing

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -21,6 +21,65 @@
         <span></span>
         <span></span>
       </button>
+      <!-- Dropdown menu -->
+      <div class="dropdown-menu" v-if="showMenu">
+        <router-link
+          to="/"
+          class="nav-link"
+          active-class="active-link"
+          @click="resetTeam"
+        >
+          Home
+        </router-link>
+        <router-link
+          to="/news"
+          class="nav-link"
+          active-class="active-link"
+          @click="resetTeam"
+        >
+          News
+        </router-link>
+        <router-link
+          to="/teams"
+          class="nav-link"
+          active-class="active-link"
+          @click="resetTeam"
+        >
+          Teams
+        </router-link>
+        <router-link
+          to="/fixtures"
+          class="nav-link"
+          active-class="active-link"
+          @click="resetTeam"
+        >
+          Fixtures
+        </router-link>
+        <router-link
+          to="/results"
+          class="nav-link"
+          active-class="active-link"
+          @click="resetTeam"
+        >
+          Results
+        </router-link>
+        <router-link
+          to="/videos"
+          class="nav-link"
+          active-class="active-link"
+          @click="resetTeam"
+        >
+          Videos
+        </router-link>
+        <router-link
+          to="/table"
+          class="nav-link"
+          active-class="active-link"
+          @click="resetTeam"
+        >
+          Table
+        </router-link>
+      </div>
     </div>
 
     <!-- Navigation links -->
@@ -191,5 +250,27 @@ export default {
   .burger-button {
     display: block;
   }
+}
+
+.dropdown-menu {
+  position: absolute;
+  top: 50px;
+  right: 0;
+  background-color: #1f3f7f;
+  display: flex;
+  flex-direction: column;
+  border-radius: 5px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  z-index: 1001;
+}
+.dropdown-menu .nav-link {
+  color: #fff;
+  padding: 10px;
+  text-decoration: none;
+  transition: background-color 0.3s;
+}
+
+.dropdown-menu .nav-link:hover {
+  background-color: rgba(255, 255, 255, 0.1);
 }
 </style>
