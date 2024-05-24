5a37f03e0cb6a99c82ef4e813ca2b5a54f48d5d3
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2024-05-24 08:22:41 +0700

    Notification Windown functionality

--- src/components/NotificationWindow.vue
+++ src/components/NotificationWindow.vue
@@ -1,12 +1,16 @@
 <template>
-  <div>
-    <div class="notification">
+  <div class="notification">
+    <div class="info-content">
+      <h2>Welcome to Our Site</h2>
       <p>
-        This site is under construction. Some features may not work as expected.
+        Please note that the site is currently under construction. As such,
+        external links are disabled and the information provided is all dummy
+        data.
       </p>
-      <button @click="hideNotification">Close</button>
+      <div class="button-container">
+        <button @click="hideNotification">Close</button>
+      </div>
     </div>
-    <!-- Your main content here -->
   </div>
 </template>
 
@@ -26,10 +30,44 @@ export default {
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  background-color: #fff;
+  background-color: #1f3f7f;
+  color: #fff;
   border: 1px solid #ccc;
   padding: 20px;
   border-radius: 5px;
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  max-width: 80%;
+  max-height: 80%;
+  overflow: auto;
+}
+
+.button-container {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.notification button {
+  background-color: #fff;
+  color: #1f3f7f;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 3px;
+  cursor: pointer;
+  border: 1px solid #1f3f7f;
+}
+
+.info-content {
+  padding: 20px;
+  background-color: #f5f5f5;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+.info-content h2 {
+  color: #1f3f7f;
+}
+
+.info-content p {
+  color: #333;
 }
 </style>
